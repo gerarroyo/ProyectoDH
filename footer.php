ede10b7750445d0172e35eb5233bad551a8b99a9
Gerardo Arroyo <gger.arroyo@gmail.com>
2019-11-11 01:24:32 -0300

    agrego Bootstrap

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <footer>
     <div class="contenedor-footer">
       <div class="contenedor-texto">
-          <p class="texto-derechos-footer">Todos los derechos reservados</p>
+          <p class="texto-derechos-footer">Copyright (c) 2018 Copyright Holder All Rights Reserved.</p>
       </div>
       <div class="contenedor-enlace-footer">
         <a href="mailto:gger.arroyo@gmail.com">Escribinos por E-Mail.</a>
--- styles.php
+++ styles.php
@@ -1,3 +1,8 @@
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+<link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/header.css">
 <link rel="stylesheet" href="css/footer.css">
